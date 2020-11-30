c1e973f70007abe7a700631c9f4761a50b1698f3
martenumberto <marten.bosse@gmail.com>
2020-11-30 08:58:37 +0100

    Update BoardConfig.mk

--- device/qualcomm/msm8953_64/BoardConfig.mk
+++ device/qualcomm/msm8953_64/BoardConfig.mk
@@ -74,4 +74,3 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-DEVICE_RESOLUTION := 1920x720                 # The Resolution of your Device
